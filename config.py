a380e286bbb4943de133a72542c31fbf24520f73
qlamenn <gladcenkodenis857@gmail.com>
2026-05-04 18:24:49 +0700

    Add files via upload

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'grid-start-secret-2026'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///f1.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/f1_' + os.urandom(4).hex() + '.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
